40764b754606333137b181094ca72652910b7aaf
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-15 20:19:20 +0530

    Update 728. Self Dividing Numbers.cpp

--- 13-Math/728. Self Dividing Numbers/728. Self Dividing Numbers.cpp
+++ 13-Math/728. Self Dividing Numbers/728. Self Dividing Numbers.cpp
@@ -1,24 +1,24 @@
-1class Solution {
-2public:
-3    vector<int> selfDividingNumbers(int left, int right) {
-4        vector<int> result;
-5        for(int i=left; i <=right; i++){
-6            if (isSelfDividing(i)) {
-7                result.push_back(i);
-8            }
-9        }
-10        return result;
-11    }
-12private:
-13    bool isSelfDividing(int num) {
-14        int temp = num; 
-15        while (temp > 0) {
-16            int digit = temp % 10;   //accessing last digit of the number
-17            if (digit == 0 || num % digit != 0) {
-18                return false;
-19            }
-20            temp /= 10; 
-21        }
-22        return true;
-23    }    
-24};
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> result;
+        for(int i=left; i <=right; i++){
+            if (isSelfDividing(i)) {
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+private:
+    bool isSelfDividing(int num) {
+        int temp = num; 
+        while (temp > 0) {
+            int digit = temp % 10;   //accessing last digit of the number
+            if (digit == 0 || num % digit != 0) {
+                return false;
+            }
+            temp /= 10; 
+        }
+        return true;
+    }    
+};
