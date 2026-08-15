2d1020e0fb8a02bcfbac0008ad2085a91dc744ed
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-15 11:36:40 +0530

    Update 3498. Reverse Degree of a String.cpp

--- 04-String-Manipulation/3498. Reverse Degree of a String/3498. Reverse Degree of a String.cpp
+++ 04-String-Manipulation/3498. Reverse Degree of a String/3498. Reverse Degree of a String.cpp
@@ -1,12 +1,12 @@
-1class Solution {
-2public:
-3    int reverseDegree(string s) {
-4        int sum = 0;
-5
-6        for (int i = 0; i < s.size(); i++) {
-7            sum += (26 - (s[i] - 'a')) * (i + 1);
-8        }
-9
-10        return sum;
-11    }
-12};
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            sum += (26 - (s[i] - 'a')) * (i + 1);
+        }
+
+        return sum;
+    }
+};
