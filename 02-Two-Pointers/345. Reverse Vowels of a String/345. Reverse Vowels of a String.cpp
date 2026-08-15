5051a98c0c4d48dcd467e39d00089755302e9ba8
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-15 11:36:12 +0530

    Update 345. Reverse Vowels of a String.cpp

--- 02-Two-Pointers/345. Reverse Vowels of a String/345. Reverse Vowels of a String.cpp
+++ 02-Two-Pointers/345. Reverse Vowels of a String/345. Reverse Vowels of a String.cpp
@@ -1,17 +1,17 @@
-1class Solution {
-2public:
-3    string reverseVowels(string s) {
-4        int left =0;
-5        int right =s.length()-1;
-6        while(left<right){
-7            while(left<right && !isvowel(s[left])) left++;
-8            while(left<right && !isvowel(s[right])) right--;
-9            swap(s[left++], s[right--]);
-10        }
-11        return s;
-12    }
-13private:
-14    bool isvowel(char c){
-15        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'; 
-16    }    
-17};
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int left =0;
+        int right =s.length()-1;
+        while(left<right){
+            while(left<right && !isvowel(s[left])) left++;
+            while(left<right && !isvowel(s[right])) right--;
+            swap(s[left++], s[right--]);
+        }
+        return s;
+    }
+private:
+    bool isvowel(char c){
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'; 
+    }    
+};
