996d2decd479a1a2b3c0a4f02ab3ec77e956f1c0
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-15 11:37:26 +0530

    Fix formatting and ensure proper code structure

--- 04-String-Manipulation/58. Length of Last Word/58. Length of Last Word.cpp
+++ 04-String-Manipulation/58. Length of Last Word/58. Length of Last Word.cpp
@@ -1,20 +1,20 @@
-1class Solution 
-2{
-3    public:
-4        int lengthOfLastWord(string s) 
-5        {
-6            int count = 0;
-7            for (int i = s.length() - 1; i >= 0; i--) 
-8            {
-9                if (s[i] != ' ') 
-10                {
-11                    count++;
-12                }
-13                else if (s[i] == ' ' && count > 0) 
-14                {
-15                return count;
-16                }
-17            }
-18        return count;
-19        }
-20};
+class Solution 
+{
+    public:
+        int lengthOfLastWord(string s) 
+        {
+            int count = 0;
+            for (int i = s.length() - 1; i >= 0; i--) 
+            {
+                if (s[i] != ' ') 
+                {
+                    count++;
+                }
+                else if (s[i] == ' ' && count > 0) 
+                {
+                return count;
+                }
+            }
+        return count;
+        }
+};
