bd62134a60852ab06e270e57e0307f17135196d1
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-15 12:06:39 +0530

    Update 367. Valid Perfect Square.cpp

--- 05-Sorting-and-Searching/367. Valid Perfect Square/367. Valid Perfect Square.cpp
+++ 05-Sorting-and-Searching/367. Valid Perfect Square/367. Valid Perfect Square.cpp
@@ -1,18 +1,18 @@
-1class Solution {
-2public:
-3    bool isPerfectSquare(int num) {
-4        long long int l =1;
-5        long long int r = num;
-6        while(l<=r){
-7            long long int mid = l+(r-l)/2;
-8            long long int sq = mid*mid;
-9            if(sq==num)
-10                return true;
-11            else if(sq<num)
-12                l = mid+1;
-13            else 
-14                r = mid-1;        
-15        }
-16        return false;
-17    }
-18};
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long int l =1;
+        long long int r = num;
+        while(l<=r){
+            long long int mid = l+(r-l)/2;
+            long long int sq = mid*mid;
+            if(sq==num)
+                return true;
+            else if(sq<num)
+                l = mid+1;
+            else 
+                r = mid-1;        
+        }
+        return false;
+    }
+};
