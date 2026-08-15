0c08a3ad227c141d9763451cf86d885de0c27d42
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-15 12:14:53 +0530

    Update 74. Search a 2D Matrix.cpp

--- 01-Arrays-and-Hashing/74. Search a 2D Matrix/74. Search a 2D Matrix.cpp
+++ 01-Arrays-and-Hashing/74. Search a 2D Matrix/74. Search a 2D Matrix.cpp
@@ -1,27 +1,26 @@
-1class Solution {
-2public:
-3    bool searchMatrix(vector<vector<int>>& matrix, int target) { //Using binary search technique for O(log(m*n))
-4        int m = matrix.size();
-5        if (m == 0) return false;
-6        int n = matrix[0].size();
-7
-8        int low = 0;
-9        int high = (m * n) - 1;
-10
-11        while (low <= high) {
-12            int mid = low + (high - low) / 2;
-13            int row = mid / n;
-14            int col = mid % n;
-15            int midValue = matrix[row][col];
-16            if (midValue == target) {
-17                return true; 
-18            } else if (midValue < target) {
-19                low = mid + 1; 
-20            } else {
-21                high = mid - 1; 
-22            }
-23        }
-24        return false;
-25    }
-26};
-27
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) { //Using binary search technique for O(log(m*n))
+        int m = matrix.size();
+        if (m == 0) return false;
+        int n = matrix[0].size();
+
+        int low = 0;
+        int high = (m * n) - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            int midValue = matrix[row][col];
+            if (midValue == target) {
+                return true; 
+            } else if (midValue < target) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1; 
+            }
+        }
+        return false;
+    }
+};
