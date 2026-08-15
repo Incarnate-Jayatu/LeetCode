e686f8cfcdb162abb383a34a1cf71820e1455be9
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-15 11:32:09 +0530

    Update 3925. Concatenate Array With Reverse.cpp

--- 01-Arrays-and-Hashing/3925. Concatenate Array With Reverse/3925. Concatenate Array With Reverse.cpp
+++ 01-Arrays-and-Hashing/3925. Concatenate Array With Reverse/3925. Concatenate Array With Reverse.cpp
@@ -1,14 +1,14 @@
-1class Solution {
-2public:
-3    vector<int> concatWithReverse(vector<int>& nums) {
-4        int n = nums.size();
-5        vector<int> ans(2 * n);
-6        for (int i = 0; i < n; i++) {
-7            ans[i] = nums[i];
-8        }
-9        for (int i = 0; i < n; i++) {
-10            ans[n + i] = nums[n - 1 - i];
-11        }
-12        return ans;
-13    }
-14};
+class Solution {
+public:
+    vector<int> concatWithReverse(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2 * n);
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+        }
+        for (int i = 0; i < n; i++) {
+            ans[n + i] = nums[n - 1 - i];
+        }
+        return ans;
+    }
+};
