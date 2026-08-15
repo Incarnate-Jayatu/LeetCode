db650534d23b1da786498f587720d3dd714f5d7f
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-15 11:30:28 +0530

    Update 1929. Concatenation of Array.cpp

--- 01-Arrays-and-Hashing/1929. Concatenation of Array/1929. Concatenation of Array.cpp
+++ 01-Arrays-and-Hashing/1929. Concatenation of Array/1929. Concatenation of Array.cpp
@@ -1,12 +1,12 @@
-1class Solution {
-2public:
-3    vector<int> getConcatenation(vector<int>& nums) {
-4        int n = nums.size();
-5        vector<int> ans(2 * n);
-6        for (int i = 0; i < n; i++) {
-7            ans[i] = nums[i];
-8            ans[i + n] = nums[i];
-9        }
-10        return ans;
-11    }
-12};
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2 * n);
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+};
