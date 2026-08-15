3a99a96046ac1958f3ebf81750580a5b2b88033f
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-15 22:25:59 +0530

    Update 202. Happy Number.cpp

--- 01-Arrays-and-Hashing/202. Happy Number/202. Happy Number.cpp
+++ 01-Arrays-and-Hashing/202. Happy Number/202. Happy Number.cpp
@@ -1,21 +1,21 @@
-1class Solution {
-2private:
-3    int getNext(int n) {
-4        int totalSum = 0;
-5        while (n > 0) {
-6            int d = n % 10;
-7            n = n / 10;
-8            totalSum += d * d;
-9        }
-10        return totalSum;
-11    }
-12public:
-13    bool isHappy(int n) {
-14        std::unordered_set<int> seen;
-15        while (n != 1 && seen.find(n) == seen.end()) {
-16            seen.insert(n);
-17            n = getNext(n);
-18        }
-19        return n == 1;
-20    }
-21};
+class Solution {
+private:
+    int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+public:
+    bool isHappy(int n) {
+        std::unordered_set<int> seen;
+        while (n != 1 && seen.find(n) == seen.end()) {
+            seen.insert(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+};
