8ef27115f2c504412bcbaef521cb701e4b87e7eb
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-18 22:46:35 +0530

    Update 832. Flipping an Image.cpp

--- 01-Arrays-and-Hashing/832. Flipping an Image/832. Flipping an Image.cpp
+++ 01-Arrays-and-Hashing/832. Flipping an Image/832. Flipping an Image.cpp
@@ -1,27 +1,26 @@
-1class Solution {
-2public:
-3    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
-4        int n = image.size();
-5        
-6        for (int i = 0; i < n; i++) {
-7            int left = 0;
-8            int right = n - 1;
-9            
-10            while (left <= right) {
-11                if (image[i][left] == image[i][right]) {
-12                    if (image[i][left] == 0) {
-13                        image[i][left] = 1;
-14                        image[i][right] = 1;
-15                    } else {
-16                        image[i][left] = 0;
-17                        image[i][right] = 0;
-18                    }
-19                }
-20                left++;
-21                right--;
-22            }
-23        }
-24        return image;
-25    }
-26};
-27
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        int n = image.size();
+        
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            
+            while (left <= right) {
+                if (image[i][left] == image[i][right]) {
+                    if (image[i][left] == 0) {
+                        image[i][left] = 1;
+                        image[i][right] = 1;
+                    } else {
+                        image[i][left] = 0;
+                        image[i][right] = 0;
+                    }
+                }
+                left++;
+                right--;
+            }
+        }
+        return image;
+    }
+};
