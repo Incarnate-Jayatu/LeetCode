49887ae1516d7076a205d67af29be7a95df5078c
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-18 22:52:47 +0530

    Update 48. Rotate Image.cpp

--- 01-Arrays-and-Hashing/48. Rotate Image/48. Rotate Image.cpp
+++ 01-Arrays-and-Hashing/48. Rotate Image/48. Rotate Image.cpp
@@ -1,16 +1,16 @@
-1class Solution {
-2public:
-3    void rotate(vector<vector<int>>& matrix) {
-4        int n = matrix.size();
-5        for (int i = 0; i < n >> 1; ++i) {
-6            for (int j = 0; j < n; ++j) {
-7                swap(matrix[i][j], matrix[n - i - 1][j]);
-8            }
-9        }
-10        for (int i = 0; i < n; ++i) {
-11            for (int j = 0; j < i; ++j) {
-12                swap(matrix[i][j], matrix[j][i]);
-13            }
-14        }
-15    }
-16};
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n >> 1; ++i) {
+            for (int j = 0; j < n; ++j) {
+                swap(matrix[i][j], matrix[n - i - 1][j]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+};
