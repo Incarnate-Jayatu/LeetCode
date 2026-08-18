1eca281771127c60db12b5152e6110356e7992fb
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-18 22:47:21 +0530

    Update 0000. Sqrtx.cpp

--- 05-Sorting-and-Searching/0000. Sqrtx/0000. Sqrtx.cpp
+++ 05-Sorting-and-Searching/0000. Sqrtx/0000. Sqrtx.cpp
@@ -1,23 +1,23 @@
-1class Solution {
-2public:
-3    int mySqrt(int x) {
-4        if (x == 0 || x == 1) return x;
-5        int start = 1;
-6        int end = x;
-7        int ans = 0;
-8        while (start <= end) {
-9            int mid = start + (end - start) / 2;
-10            if (mid == x / mid) {
-11                return mid;
-12            } 
-13            else if (mid < x / mid) {
-14                ans = mid;     
-15                start = mid + 1; 
-16            } 
-17            else {
-18                end = mid - 1;   
-19            }
-20        }
-21        return ans;
-22    }
-23};
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+        int start = 1;
+        int end = x;
+        int ans = 0;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid == x / mid) {
+                return mid;
+            } 
+            else if (mid < x / mid) {
+                ans = mid;     
+                start = mid + 1; 
+            } 
+            else {
+                end = mid - 1;   
+            }
+        }
+        return ans;
+    }
+};
