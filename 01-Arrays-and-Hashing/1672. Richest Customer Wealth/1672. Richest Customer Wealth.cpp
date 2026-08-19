249b1da2ab077db0043769b7f06051a3e0d13cbf
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-19 18:10:11 +0530

    Update 1672. Richest Customer Wealth.cpp

--- 01-Arrays-and-Hashing/1672. Richest Customer Wealth/1672. Richest Customer Wealth.cpp
+++ 01-Arrays-and-Hashing/1672. Richest Customer Wealth/1672. Richest Customer Wealth.cpp
@@ -1,18 +1,18 @@
-1class Solution {
-2public:
-3    int maximumWealth(vector<vector<int>>& accounts) {
-4        int max=0, sum=0;
-5        int m = accounts.size();       // Number of customers (rows)
-6        int n = accounts[0].size();    // Number of banks (columns)
-7        for(int i=0; i<m; i++){
-8            sum=0;
-9            for(int j=0; j<n; j++){
-10                sum+=accounts[i][j];
-11                if(sum > max){
-12                    max = sum;
-13                }
-14            }
-15        }
-16        return max;
-17    }
-18};
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max=0, sum=0;
+        int m = accounts.size();       // Number of customers (rows)
+        int n = accounts[0].size();    // Number of banks (columns)
+        for(int i=0; i<m; i++){
+            sum=0;
+            for(int j=0; j<n; j++){
+                sum+=accounts[i][j];
+                if(sum > max){
+                    max = sum;
+                }
+            }
+        }
+        return max;
+    }
+};
