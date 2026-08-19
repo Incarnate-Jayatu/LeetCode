a2a41e17baef7e63877b78719b3eb9dea567dcd4
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-19 18:41:03 +0530

    Update 1480. Running Sum of 1d Array.cpp

--- 01-Arrays-and-Hashing/1480. Running Sum of 1d Array/1480. Running Sum of 1d Array.cpp
+++ 01-Arrays-and-Hashing/1480. Running Sum of 1d Array/1480. Running Sum of 1d Array.cpp
@@ -1,14 +1,13 @@
-1class Solution {
-2public:
-3    vector<int> runningSum(vector<int>& nums) {
-4        int n = nums.size();
-5        vector<int> result(n, 0); 
-6        for (int i = 0; i < n; i++) {
-7            for (int j = 0; j <= i; j++) {
-8                result[i] += nums[j];
-9            }
-10        }
-11        return result;
-12    }
-13};
-14
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n, 0); 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                result[i] += nums[j];
+            }
+        }
+        return result;
+    }
+};
