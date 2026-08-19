8c9a722d32b1f840167fe4138cdadced3a7cc791
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-19 18:20:57 +0530

    [2824] [Time Beats: 100.00%] [Memory Beats: 87.18%] - Leet2Hub

--- 01-Arrays-and-Hashing/2824. Count Pairs Whose Sum is Less than Target/2824. Count Pairs Whose Sum is Less than Target.cpp
+++ 01-Arrays-and-Hashing/2824. Count Pairs Whose Sum is Less than Target/2824. Count Pairs Whose Sum is Less than Target.cpp
@@ -1,15 +1,15 @@
-class Solution {
-public:
-    int countPairs(vector<int>& nums, int target) {
-        int count=0;
-        for(int i=0; i<nums.size(); i++){
-            int sum =0;
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[i]+nums[j] < target){
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-};
+1class Solution {
+2public:
+3    int countPairs(vector<int>& nums, int target) {
+4        int count=0;
+5        for(int i=0; i<nums.size(); i++){
+6            int sum =0;
+7            for(int j=i+1; j<nums.size(); j++){
+8                if(nums[i]+nums[j] < target){
+9                    count++;
+10                }
+11            }
+12        }
+13        return count;
+14    }
+15};
