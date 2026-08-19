86325124957b1df9fd581adcb8f5b36618d7f826
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-19 21:48:21 +0530

    Update 283. Move Zeroes.cpp

--- 01-Arrays-and-Hashing/283. Move Zeroes/283. Move Zeroes.cpp
+++ 01-Arrays-and-Hashing/283. Move Zeroes/283. Move Zeroes.cpp
@@ -1,12 +1,12 @@
-1class Solution {
-2public:
-3    void moveZeroes(vector<int>& nums) {
-4        int j=0;
-5        for(int i=0;i<nums.size();i++){
-6            if (nums[i]!=0){
-7                swap(nums[i],nums[j]);
-8                j++;
-9            }
-10        }
-11    }
-12};
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if (nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+    }
+};
