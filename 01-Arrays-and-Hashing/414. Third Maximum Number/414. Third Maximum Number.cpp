9c4fa7272155703ff143ead487820393335bc60c
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-19 22:36:59 +0530

    Update 414. Third Maximum Number.cpp

--- 01-Arrays-and-Hashing/414. Third Maximum Number/414. Third Maximum Number.cpp
+++ 01-Arrays-and-Hashing/414. Third Maximum Number/414. Third Maximum Number.cpp
@@ -1,28 +1,27 @@
-1class Solution {
-2public:
-3    int thirdMax(vector<int>& arr) {
-4        int n = arr.size();
-5        for (int i = 1; i < n; i++) {
-6            int key = arr[i];
-7            int j = i - 1;
-8            while (j >= 0 && arr[j] < key) {
-9                arr[j + 1] = arr[j];
-10                j--;
-11            }
-12            arr[j + 1] = key;
-13        }
-14        
-15        int distinct_count = 1;
-16        int result = arr[0];
-17        for (int i = 1; i < n; i++) {
-18            if (arr[i] != arr[i - 1]) {
-19                distinct_count++;
-20            }
-21            if (distinct_count == 3) {
-22                return arr[i];
-23            }
-24        }
-25        return arr[0]; 
-26    }
-27};
-28
+class Solution {
+public:
+    int thirdMax(vector<int>& arr) {
+        int n = arr.size();
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] < key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        
+        int distinct_count = 1;
+        int result = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] != arr[i - 1]) {
+                distinct_count++;
+            }
+            if (distinct_count == 3) {
+                return arr[i];
+            }
+        }
+        return arr[0]; 
+    }
+};
