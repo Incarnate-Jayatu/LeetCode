c2325712e2edbbe4ae5c91d6d39f2d98c0a40191
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-23 13:06:58 +0530

    Update 1927. Sum Game.cpp

--- 04-String-Manipulation/1927. Sum Game/1927. Sum Game.cpp
+++ 04-String-Manipulation/1927. Sum Game/1927. Sum Game.cpp
@@ -1,25 +1,24 @@
-1class Solution {
-2public:
-3    bool sumGame(std::string num) {
-4        int n = num.length();
-5        int sum_diff = 0;
-6        int q_diff = 0;
-7        for (int i = 0; i < n; i++) {
-8            if (i < n / 2) {
-9                if (num[i] == '?') {
-10                    q_diff++;
-11                } else {
-12                    sum_diff += (num[i] - '0');
-13                }
-14            } else {
-15                if (num[i] == '?') {
-16                    q_diff--;
-17                } else {
-18                    sum_diff -= (num[i] - '0');
-19                }
-20            }
-21        }
-22        return (sum_diff * 2 != -9 * q_diff);
-23    }
-24};
-25
+class Solution {
+public:
+    bool sumGame(std::string num) {
+        int n = num.length();
+        int sum_diff = 0;
+        int q_diff = 0;
+        for (int i = 0; i < n; i++) {
+            if (i < n / 2) {
+                if (num[i] == '?') {
+                    q_diff++;
+                } else {
+                    sum_diff += (num[i] - '0');
+                }
+            } else {
+                if (num[i] == '?') {
+                    q_diff--;
+                } else {
+                    sum_diff -= (num[i] - '0');
+                }
+            }
+        }
+        return (sum_diff * 2 != -9 * q_diff);
+    }
+};
