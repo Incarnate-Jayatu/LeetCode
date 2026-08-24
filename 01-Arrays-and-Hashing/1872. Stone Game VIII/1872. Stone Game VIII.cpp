b9f5703d88fcebfd8dd99c7ae03f7e1923560839
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-24 22:31:20 +0530

    Update 1872. Stone Game VIII.cpp

--- 01-Arrays-and-Hashing/1872. Stone Game VIII/1872. Stone Game VIII.cpp
+++ 01-Arrays-and-Hashing/1872. Stone Game VIII/1872. Stone Game VIII.cpp
@@ -1,15 +1,14 @@
-1class Solution {
-2public:
-3    int stoneGameVIII(vector<int>& stones) {
-4        int n = stones.size();
-5        for (int i = 1; i < n; ++i) {
-6            stones[i] += stones[i - 1];
-7        }
-8        int max_diff = stones[n - 1];
-9        for (int i = n - 2; i >= 1; --i) {
-10            max_diff = max(max_diff, stones[i] - max_diff);
-11        }
-12        return max_diff;
-13    }
-14};
-15
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        for (int i = 1; i < n; ++i) {
+            stones[i] += stones[i - 1];
+        }
+        int max_diff = stones[n - 1];
+        for (int i = n - 2; i >= 1; --i) {
+            max_diff = max(max_diff, stones[i] - max_diff);
+        }
+        return max_diff;
+    }
+};
