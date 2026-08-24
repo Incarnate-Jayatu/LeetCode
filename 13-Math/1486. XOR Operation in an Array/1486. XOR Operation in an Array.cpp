63293cfcde52c3e81d9e49b8fe3c8662832f8ca4
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-24 22:43:53 +0530

    Update 1486. XOR Operation in an Array.cpp

--- 13-Math/1486. XOR Operation in an Array/1486. XOR Operation in an Array.cpp
+++ 13-Math/1486. XOR Operation in an Array/1486. XOR Operation in an Array.cpp
@@ -1,10 +1,10 @@
-1class Solution {
-2public:
-3    int xorOperation(int n, int start) {
-4        int res = 0;
-5        for (int i = 0; i < n; ++i) {
-6            res ^= (start + 2 * i);
-7        }
-8        return res;
-9    }
-10};
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            res ^= (start + 2 * i);
+        }
+        return res;
+    }
+};
