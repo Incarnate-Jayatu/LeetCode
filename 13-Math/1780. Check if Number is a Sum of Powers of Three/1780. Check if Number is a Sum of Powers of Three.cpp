df13323bf673625cc56d45ad0971744974f30dd8
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-25 20:36:00 +0530

    Update 1780. Check if Number is a Sum of Powers of Three.cpp

--- 13-Math/1780. Check if Number is a Sum of Powers of Three/1780. Check if Number is a Sum of Powers of Three.cpp
+++ 13-Math/1780. Check if Number is a Sum of Powers of Three/1780. Check if Number is a Sum of Powers of Three.cpp
@@ -1,13 +1,12 @@
-1class Solution {
-2public:
-3    bool checkPowersOfThree(int n) {
-4        while (n > 0) {
-5            if (n % 3 == 2) {
-6                return false;
-7            }
-8            n /= 3;
-9        }
-10        return true;
-11    }
-12};
-13
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+};
