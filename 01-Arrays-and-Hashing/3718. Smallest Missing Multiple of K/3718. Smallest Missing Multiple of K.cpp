18cf79a76877f0bbc60772bcd4d19739c5db3853
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-25 18:04:14 +0530

    Update 3718. Smallest Missing Multiple of K.cpp

--- 01-Arrays-and-Hashing/3718. Smallest Missing Multiple of K/3718. Smallest Missing Multiple of K.cpp
+++ 01-Arrays-and-Hashing/3718. Smallest Missing Multiple of K/3718. Smallest Missing Multiple of K.cpp
@@ -1,13 +1,13 @@
-1class Solution {
-2public:
-3    int missingMultiple(std::vector<int>& nums, int k) {
-4        std::sort(nums.begin(), nums.end());
-5        int current_multiple = k;
-6        for (int num : nums) {
-7            if (num == current_multiple) {
-8                current_multiple += k;
-9            }
-10        }
-11        return current_multiple;
-12    }
-13};
+class Solution {
+public:
+    int missingMultiple(std::vector<int>& nums, int k) {
+        std::sort(nums.begin(), nums.end());
+        int current_multiple = k;
+        for (int num : nums) {
+            if (num == current_multiple) {
+                current_multiple += k;
+            }
+        }
+        return current_multiple;
+    }
+};
