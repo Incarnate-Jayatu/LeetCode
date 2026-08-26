97dc9bd71af2903ee9e67a2c79cf27e124484335
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-26 19:59:43 +0530

    Update 2904. Shortest and Lexicographically Smallest Beautiful String.cpp

--- 04-String-Manipulation/2904. Shortest and Lexicographically Smallest Beautiful String/2904. Shortest and Lexicographically Smallest Beautiful String.cpp
+++ 04-String-Manipulation/2904. Shortest and Lexicographically Smallest Beautiful String/2904. Shortest and Lexicographically Smallest Beautiful String.cpp
@@ -1,19 +1,17 @@
-1class Solution {
-2public:
-3    string shortestBeautifulSubstring(string s, int k) {
-4        int bestLeft = -1, minLength = s.length() + 1, ones = 0;
-5        for (int l = 0, r = 0; r < s.length(); ++r) {
-6            if (s[r] == '1') ++ones;
-7            while (ones == k) {
-8                int currentLen = r - l + 1;
-9                if (currentLen < minLength || (currentLen == minLength && (bestLeft == -1 || s.compare(l, currentLen, s, bestLeft, minLength) < 0))) {
-10                    bestLeft = l;
-11                    minLength = currentLen;
-12                }
-13                if (s[l++] == '1') --ones;
-14            }
-15        }
-16        return bestLeft == -1 ?  : s.substr(bestLeft, minLength);
-17    }
-18};
-19
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int bestLeft = -1, minLength = s.length() + 1, ones = 0;
+        for (int l = 0, r = 0; r < s.length(); ++r) {
+            if (s[r] == '1') ++ones;
+            while (ones == k) {
+                int currentLen = r - l + 1;
+                if (currentLen < minLength || (currentLen == minLength && (bestLeft == -1 || s.compare(l, currentLen, s, bestLeft, minLength) < 0))) {
+                    bestLeft = l;
+                    minLength = currentLen;
+                }
+                if (s[l++] == '1') --ones;
+            }
+        }        return bestLeft == -1 ?  : s.substr(bestLeft, minLength);
+    }
+};
