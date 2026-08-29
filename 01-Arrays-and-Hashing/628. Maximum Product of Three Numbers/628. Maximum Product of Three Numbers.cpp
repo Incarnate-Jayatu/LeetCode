008ebc2dc8193ced589aaec8c97559d7646b4aa3
Jayatu Bhattad <jayatu29@gmail.com>
2026-08-29 19:21:25 +0530

    Update 628. Maximum Product of Three Numbers.cpp

--- 01-Arrays-and-Hashing/628. Maximum Product of Three Numbers/628. Maximum Product of Three Numbers.cpp
+++ 01-Arrays-and-Hashing/628. Maximum Product of Three Numbers/628. Maximum Product of Three Numbers.cpp
@@ -1,11 +1,10 @@
-1class Solution {
-2public:
-3    int maximumProduct(vector<int>& nums) {
-4        int n = nums.size();
-5        sort(nums.begin(), nums.end());
-6        int m1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
-7        int m2 = nums[0] * nums[1] * nums[n - 1];
-8        return max(m1, m2);
-9    }
-10};
-11
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int m1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+        int m2 = nums[0] * nums[1] * nums[n - 1];
+        return max(m1, m2);
+    }
+};
