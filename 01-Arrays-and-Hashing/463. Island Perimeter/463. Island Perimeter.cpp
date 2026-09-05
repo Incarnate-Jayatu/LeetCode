7fd401231ad0c84edffa82b2843148c9d07eb876
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-05 23:03:43 +0530

    Fix formatting issues in Island Perimeter.cpp

--- 01-Arrays-and-Hashing/463. Island Perimeter/463. Island Perimeter.cpp
+++ 01-Arrays-and-Hashing/463. Island Perimeter/463. Island Perimeter.cpp
@@ -1,23 +1,23 @@
-1class Solution {
-2public:
-3    int islandPerimeter(std::vector<std::vector<int>>& grid) {
-4        int islands = 0;
-5        int neighbors = 0;
-6        int rows = grid.size();
-7        int cols = grid[0].size();
-8        for (int i = 0; i < rows; ++i) {
-9            for (int j = 0; j < cols; ++j) {
-10                if (grid[i][j] == 1) {
-11                    islands++;
-12                    if (i > 0 && grid[i - 1][j] == 1) {
-13                        neighbors++;
-14                    }
-15                    if (j > 0 && grid[i][j - 1] == 1) {
-16                        neighbors++;
-17                    }
-18                }
-19            }
-20        }
-21        return (islands * 4) - (neighbors * 2);
-22    }
-23};
+class Solution {
+public:
+    int islandPerimeter(std::vector<std::vector<int>>& grid) {
+        int islands = 0;
+        int neighbors = 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == 1) {
+                    islands++;
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        neighbors++;
+                    }
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        neighbors++;
+                    }
+                }
+            }
+        }
+        return (islands * 4) - (neighbors * 2);
+    }
+};
