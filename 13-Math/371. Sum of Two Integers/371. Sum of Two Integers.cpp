171319135f69a4da5e0ed61f9abfbe4de1580267
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-09 21:04:46 +0530

    [371] [Time Beats: 100.00%] [Memory Beats: 1.45%] - Leet2Hub

--- 13-Math/371. Sum of Two Integers/371. Sum of Two Integers.cpp
+++ 13-Math/371. Sum of Two Integers/371. Sum of Two Integers.cpp
@@ -1,11 +1,13 @@
 1class Solution {
 2public:
 3    int getSum(int a, int b) {
-4        while (b != 0) {
-5            unsigned int carry = a & b;
-6            a = a ^ b;
-7            b = carry << 1;
-8        }
-9        return a;
-10    }
-11};
+4        
+5        while (b != 0) {
+6            unsigned int carry = a & b;
+7            a = a ^ b;
+8            b = carry << 1;
+9        }
+10
+11        return a;
+12    }
+13};
