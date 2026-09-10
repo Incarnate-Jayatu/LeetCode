fb3bb80ef2e4ac09206f6c30dbc4e3880e5e4671
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-10 19:28:12 +0530

    Update 203. Remove Linked List Elements.cpp

--- 08-Linked-List/203. Remove Linked List Elements/203. Remove Linked List Elements.cpp
+++ 08-Linked-List/203. Remove Linked List Elements/203. Remove Linked List Elements.cpp
@@ -1,30 +1,30 @@
-1/**
-2 * Definition for singly-linked list.
-3 * struct ListNode {
-4 *     int val;
-5 *     ListNode *next;
-6 *     ListNode() : val(0), next(nullptr) {}
-7 *     ListNode(int x) : val(x), next(nullptr) {}
-8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-9 * };
-10 */
-11class Solution {
-12public:
-13    ListNode* removeElements(ListNode* head, int val){
-14        ListNode* dummy = new ListNode(0);
-15        dummy->next = head;
-16        ListNode* curr = dummy;
-17        while (curr->next != nullptr) {
-18            if (curr->next->val == val) {
-19                ListNode* toDelete = curr->next;
-20                curr->next = curr->next->next; 
-21                delete toDelete; 
-22            } else {
-23                curr = curr->next;
-24            }
-25        }
-26        ListNode* newHead = dummy->next;
-27        delete dummy;
-28        return newHead;
-29    }
-30};
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val){
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* curr = dummy;
+        while (curr->next != nullptr) {
+            if (curr->next->val == val) {
+                ListNode* toDelete = curr->next;
+                curr->next = curr->next->next; 
+                delete toDelete; 
+            } else {
+                curr = curr->next;
+            }
+        }
+        ListNode* newHead = dummy->next;
+        delete dummy;
+        return newHead;
+    }
+};
