ab2b037b6fd97efbb2155f5e46768e0982ad9586
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-11 23:28:19 +0530

    Update 3483. Unique 3 Digit Even Numbers.cpp

--- 01-Arrays-and-Hashing/3483. Unique 3 Digit Even Numbers /3483. Unique 3 Digit Even Numbers.cpp
+++ 01-Arrays-and-Hashing/3483. Unique 3 Digit Even Numbers /3483. Unique 3 Digit Even Numbers.cpp
@@ -1,29 +1,29 @@
-1class Solution {
-2public:
-3    int totalNumbers(vector<int>& digits) {
-4        int n = digits.size();
-5        bool vis[1000]{};
-6        int ans = 0;
-7        for(int i = 0; i < n; ++i){
-8            if(digits[i] == 0){
-9                continue;
-10            }
-11            for(int j = 0; j < n; ++j){
-12                if(j == i){
-13                    continue;
-14                }
-15                for(int k = 0; k < n; ++k){
-16                    if(k == i || k == j || digits[k] % 2 != 0){
-17                        continue;
-18                    }
-19                    int x = digits[i] * 100 + digits[j] * 10 + digits[k];
-20                    if(!vis[x]){
-21                        vis[x] = true;
-22                        ++ans;
-23                    }
-24                }
-25            }
-26        }
-27        return ans;
-28    }
-29};
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        int n = digits.size();
+        bool vis[1000]{};
+        int ans = 0;
+        for(int i = 0; i < n; ++i){
+            if(digits[i] == 0){
+                continue;
+            }
+            for(int j = 0; j < n; ++j){
+                if(j == i){
+                    continue;
+                }
+                for(int k = 0; k < n; ++k){
+                    if(k == i || k == j || digits[k] % 2 != 0){
+                        continue;
+                    }
+                    int x = digits[i] * 100 + digits[j] * 10 + digits[k];
+                    if(!vis[x]){
+                        vis[x] = true;
+                        ++ans;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
