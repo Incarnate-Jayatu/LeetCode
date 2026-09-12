46fd1694b4ec920db148fc72c80b12e24a2681ba
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-12 13:49:38 +0530

    Update 148. Sort List.cpp

--- 02-Two-Pointers/148. Sort List/148. Sort List.cpp
+++ 02-Two-Pointers/148. Sort List/148. Sort List.cpp
@@ -1,51 +1,51 @@
-1/**
-2 * Definition for singly-linked list.
-3 * struct ListNode {
-4 *     int val;
-5 *     ListNode *next;
-6 *     ListNode() : val(0), next(nullptr) {}
-7 *     ListNode(int x) : val(x), next(nullptr) {}
-8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-9 * };
-10 */
-11class Solution {
-12private:
-13    ListNode* getMid(ListNode* head) {
-14        ListNode* slow = head;
-15        ListNode* fast = head->next;
-16        while (fast != nullptr && fast->next != nullptr) {
-17            slow = slow->next;
-18            fast = fast->next->next;
-19        }
-20        return slow;
-21    }
-22    ListNode* merge(ListNode* list1, ListNode* list2) {
-23        ListNode dummy(0);
-24        ListNode* tail = &dummy;
-25        while (list1 != nullptr && list2 != nullptr) {
-26            if (list1->val < list2->val) {
-27                tail->next = list1;
-28                list1 = list1->next;
-29            } else {
-30                tail->next = list2;
-31                list2 = list2->next;
-32            }
-33            tail = tail->next;
-34        }
-35        tail->next = (list1 != nullptr) ? list1 : list2;
-36        return dummy.next;
-37    }
-38public:
-39    ListNode* sortList(ListNode* head) {
-40        if (head == nullptr || head->next == nullptr) {
-41            return head;
-42        }
-43        ListNode* mid = getMid(head);
-44        ListNode* left = head;
-45        ListNode* right = mid->next;
-46        mid->next = nullptr;
-47        left = sortList(left);
-48        right = sortList(right);
-49        return merge(left, right);
-50    }
-51};
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* getMid(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val < list2->val) {
+                tail->next = list1;
+                list1 = list1->next;
+            } else {
+                tail->next = list2;
+                list2 = list2->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = (list1 != nullptr) ? list1 : list2;
+        return dummy.next;
+    }
+public:
+    ListNode* sortList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode* mid = getMid(head);
+        ListNode* left = head;
+        ListNode* right = mid->next;
+        mid->next = nullptr;
+        left = sortList(left);
+        right = sortList(right);
+        return merge(left, right);
+    }
+};
