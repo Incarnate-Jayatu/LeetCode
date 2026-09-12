1a017196ff3116454e790bdf40b9d727638a9bc9
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-12 13:33:55 +0530

    Update 75. Sort Colors.cpp

--- 01-Arrays-and-Hashing/75. Sort Colors/75. Sort Colors.cpp
+++ 01-Arrays-and-Hashing/75. Sort Colors/75. Sort Colors.cpp
@@ -1,15 +1,15 @@
-1class Solution {
-2public:
-3    void sortColors(vector<int>& arr){
-4        int n = arr.size();
-5        for(int i = 0; i < n; i++){
-6            bool swapped = false;
-7            for(int j = 0; j < n-i-1; j++){
-8                if(arr[j] > arr[j+1]){ swap(arr[j], arr[j+1]); swapped = true; }
-9            }
-10            if(!swapped) break;
-11        }
-12        for(int i=0; i<n; i++)
-13        cout<<arr[i]<< ;
-14    }
-15};
+class Solution {
+public:
+    void sortColors(vector<int>& arr){
+        int n = arr.size();
+        for(int i = 0; i < n; i++){
+            bool swapped = false;
+            for(int j = 0; j < n-i-1; j++){
+                if(arr[j] > arr[j+1]){ swap(arr[j], arr[j+1]); swapped = true; }
+            }
+            if(!swapped) break;
+        }
+        for(int i=0; i<n; i++)
+        cout<<arr[i]<< ;
+    }
+};
