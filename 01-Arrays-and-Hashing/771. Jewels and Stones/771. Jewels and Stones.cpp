046bff3b744b2f3653688bf1617b9a26453c4e36
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-13 10:50:41 +0530

    Update 771. Jewels and Stones.cpp

--- 01-Arrays-and-Hashing/771. Jewels and Stones/771. Jewels and Stones.cpp
+++ 01-Arrays-and-Hashing/771. Jewels and Stones/771. Jewels and Stones.cpp
@@ -1,13 +1,13 @@
-1class Solution {
-2public:
-3    int numJewelsInStones(string jewels, string stones) {
-4        int count=0;
-5        for(int i=0; i<jewels.length(); i++){
-6            for(int j=0; j<stones.length(); j++){
-7                if(jewels[i] == stones[j])
-8                    count++;
-9            }
-10        }
-11        return count;
-12    }
-13};
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count=0;
+        for(int i=0; i<jewels.length(); i++){
+            for(int j=0; j<stones.length(); j++){
+                if(jewels[i] == stones[j])
+                    count++;
+            }
+        }
+        return count;
+    }
+};
