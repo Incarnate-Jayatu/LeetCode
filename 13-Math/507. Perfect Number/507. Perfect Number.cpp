b23e10051759f0995f1384fac29647c8a960ab68
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-15 23:34:48 +0530

    Update 507. Perfect Number.cpp

--- 13-Math/507. Perfect Number/507. Perfect Number.cpp
+++ 13-Math/507. Perfect Number/507. Perfect Number.cpp
@@ -1,16 +1,35 @@
-1class Solution {
-2public:
-3    bool checkPerfectNumber(int num) {
-4        if (num <= 1) return false;
-5        int sum = 1;
-6        for (int i = 2; i * i <= num; i++) {
-7            if (num % i == 0) {
-8                sum += i;
-9                if (i * i != num) {
-10                    sum += num / i;
-11                }
-12            }
-13        }
-14        return sum == num;
-15    }
-16};
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if (num <= 1) return false;
+        int sum = 1;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i;
+                }
+            }
+        }
+        return sum == num;
+    }
+};
+
+/* ALTERNATE SOLUTION
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        bool result;
+        int sum=0;
+        for(int i=1; i<=num/2; i++){
+            if(num%i == 0)
+                sum += i;
+        }
+        if(sum == num)
+            result = true;
+        else
+            result =false;
+        return result;    
+    }
+};
+*/
