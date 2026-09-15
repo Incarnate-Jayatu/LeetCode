750c738e4d48311ce57289520ba5f4a870c504e5
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-15 23:26:46 +0530

    Update 509. Fibonacci Number.cpp

--- 13-Math/509. Fibonacci Number/509. Fibonacci Number.cpp
+++ 13-Math/509. Fibonacci Number/509. Fibonacci Number.cpp
@@ -1,18 +1,17 @@
-1class Solution {
-2public:
-3    int fib(int n) {
-4        if (n <= 1) {
-5            return n;
-6        }
-7        int prev2 = 0; 
-8        int prev1 = 1; 
-9        int current = 0;
-10        for (int i = 2; i <= n; ++i) {
-11            current = prev1 + prev2;
-12            prev2 = prev1;
-13            prev1 = current;
-14        }
-15        return current;
-16    }
-17};
-18
+class Solution {
+public:
+    int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int prev2 = 0; 
+        int prev1 = 1; 
+        int current = 0;
+        for (int i = 2; i <= n; ++i) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return current;
+    }
+};
