554f6f6928d9a7c1d7099f49f6566a3194635640
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-15 23:59:40 +0530

    Update 1716. Calculate Money in Leetcode Bank.cpp

--- 13-Math/1716. Calculate Money in Leetcode Bank/1716. Calculate Money in Leetcode Bank.cpp
+++ 13-Math/1716. Calculate Money in Leetcode Bank/1716. Calculate Money in Leetcode Bank.cpp
@@ -1,18 +1,17 @@
-1class Solution {
-2public:
-3    int totalMoney(int n) {
-4        int totalMoney = 0;
-5        int incrementAmt = 0; 
-6        int dailyAmt = 1;     
-7        for (int i = 1; i <= n; i++) {
-8            totalMoney += (dailyAmt + incrementAmt);
-9            dailyAmt++;
-10            if (i % 7 == 0) {
-11                incrementAmt++;
-12                dailyAmt = 1;
-13            }
-14        }
-15        return totalMoney;
-16    }
-17};
-18
+class Solution {
+public:
+    int totalMoney(int n) {
+        int totalMoney = 0;
+        int incrementAmt = 0; 
+        int dailyAmt = 1;     
+        for (int i = 1; i <= n; i++) {
+            totalMoney += (dailyAmt + incrementAmt);
+            dailyAmt++;
+            if (i % 7 == 0) {
+                incrementAmt++;
+                dailyAmt = 1;
+            }
+        }
+        return totalMoney;
+    }
+};
