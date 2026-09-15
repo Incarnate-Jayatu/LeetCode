9e4e926d9ff3f34f29d2b2b0518063724d1c55c6
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-15 22:53:22 +0530

    Update 263. Ugly Number.cpp

--- 13-Math/263. Ugly Number/263. Ugly Number.cpp
+++ 13-Math/263. Ugly Number/263. Ugly Number.cpp
@@ -1,13 +1,12 @@
-1class Solution {
-2public:
-3    bool isUgly(int n) {
-4        if (n <= 0) return false;
-5        for (int factor : {2, 3, 5}) {
-6            while (n % factor == 0) {
-7                n /= factor;
-8            }
-9        }
-10        return n == 1;
-11    }
-12};
-13
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n <= 0) return false;
+        for (int factor : {2, 3, 5}) {
+            while (n % factor == 0) {
+                n /= factor;
+            }
+        }
+        return n == 1;
+    }
+};
