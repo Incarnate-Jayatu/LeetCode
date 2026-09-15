facedc6bfb81fe9d3ea6e5fbd1ec2887c29c211d
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-15 23:49:48 +0530

    Update 1252. Cells with Odd Values in a Matrix.cpp

--- 01-Arrays-and-Hashing/1252. Cells with Odd Values in a Matrix/1252. Cells with Odd Values in a Matrix.cpp
+++ 01-Arrays-and-Hashing/1252. Cells with Odd Values in a Matrix/1252. Cells with Odd Values in a Matrix.cpp
@@ -1,20 +1,20 @@
-1class Solution {
-2public:
-3    int oddCells(int m, int n, vector<vector<int>>& indices) {
-4        vector<int> row(m, 0);
-5        vector<int> col(n, 0);
-6        for (auto& index : indices) {
-7            row[index[0]]++;
-8            col[index[1]]++;
-9        }
-10        int oddCount = 0;
-11        for (int i = 0; i < m; ++i) {
-12            for (int j = 0; j < n; ++j) {
-13                if ((row[i] + col[j]) % 2 != 0) {
-14                    oddCount++;
-15                }
-16            }
-17        }
-18        return oddCount;
-19    }
-20};
+class Solution {
+public:
+    int oddCells(int m, int n, vector<vector<int>>& indices) {
+        vector<int> row(m, 0);
+        vector<int> col(n, 0);
+        for (auto& index : indices) {
+            row[index[0]]++;
+            col[index[1]]++;
+        }
+        int oddCount = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if ((row[i] + col[j]) % 2 != 0) {
+                    oddCount++;
+                }
+            }
+        }
+        return oddCount;
+    }
+};
