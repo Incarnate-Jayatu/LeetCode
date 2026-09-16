c9440aa4768a9e20f33dfb390182dbfd136f8994
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-16 23:20:10 +0530

    Update 1662. Check If Two String Arrays are Equivalent.cpp

--- 01-Arrays-and-Hashing/1662. Check If Two String Arrays are Equivalent/1662. Check If Two String Arrays are Equivalent.cpp
+++ 01-Arrays-and-Hashing/1662. Check If Two String Arrays are Equivalent/1662. Check If Two String Arrays are Equivalent.cpp
@@ -1,15 +1,14 @@
-1class Solution {
-2public:
-3    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-4        string s1 = ;
-5        string s2 = ;
-6        for (const string& s : word1) {
-7            s1 += s;
-8        }
-9        for (const string& s : word2) {
-10            s2 += s;
-11        }
-12        return s1 == s2;
-13    }
-14};
-15
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1 = ;
+        string s2 = ;
+        for (const string& s : word1) {
+            s1 += s;
+        }
+        for (const string& s : word2) {
+            s2 += s;
+        }
+        return s1 == s2;
+    }
+};
