e5d358e9666b543f473024f67add756e130471f0
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-16 22:02:44 +0530

    Update 70. Climbing Stairs.cpp

--- 13-Math/70. Climbing Stairs/70. Climbing Stairs.cpp
+++ 13-Math/70. Climbing Stairs/70. Climbing Stairs.cpp
@@ -1,13 +1,13 @@
-1class Solution {
-2public:
-3    int climbStairs(int n) {
-4        if(n <= 2) return n;
-5        int prev2 = 1, prev1 = 2;
-6        for(int i = 3; i <= n; i++){
-7        int current = prev2 + prev1;
-8            prev2 = prev1;
-9            prev1 = current;
-10        }
-11        return prev1;
-12    }
-13};
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2) return n;
+        int prev2 = 1, prev1 = 2;
+        for(int i = 3; i <= n; i++){
+        int current = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+};
