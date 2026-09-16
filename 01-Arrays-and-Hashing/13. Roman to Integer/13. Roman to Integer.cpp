8a2c61d1f61d5978e428121641fb7e64191ceef8
Jayatu Bhattad <jayatu29@gmail.com>
2026-09-16 21:33:22 +0530

    Update 13. Roman to Integer.cpp

--- 01-Arrays-and-Hashing/13. Roman to Integer/13. Roman to Integer.cpp
+++ 01-Arrays-and-Hashing/13. Roman to Integer/13. Roman to Integer.cpp
@@ -1,33 +1,33 @@
-1class Solution
-2{
-3public:
-4    int char2num(char a)
-5    {
-6        switch(a){
-7            case 'I': return 1;
-8            case 'V': return 5;
-9            case 'X': return 10;
-10            case 'L': return 50;
-11            case 'C': return 100;
-12            case 'D': return 500;
-13            case 'M': return 1000;
-14            default: return 0;
-15        }
-16    }
-17    int romanToInt(string s)
-18    {
-19        int result = 0;
-20        for(int i = 0; i < s.length(); i++)
-21        {
-22            if(i+1<s.length() && char2num(s[i])<char2num(s[i+1])) 
-23            {
-24                result -= char2num(s[i]);
-25            }
-26            else
-27            {
-28                result += char2num(s[i]);
-29            }
-30        }
-31        return result;
-32    }
-33};
+class Solution
+{
+public:
+    int char2num(char a)
+    {
+        switch(a){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+    int romanToInt(string s)
+    {
+        int result = 0;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(i+1<s.length() && char2num(s[i])<char2num(s[i+1])) 
+            {
+                result -= char2num(s[i]);
+            }
+            else
+            {
+                result += char2num(s[i]);
+            }
+        }
+        return result;
+    }
+};
