e984f6b280a35af458d987300f654e0a46f02820
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-16 23:41:48 +0530

    Update 2129. Capitalize the Title.cpp

--- 04-String-Manipulation/2129. Capitalize the Title/2129. Capitalize the Title.cpp
+++ 04-String-Manipulation/2129. Capitalize the Title/2129. Capitalize the Title.cpp
@@ -1,20 +1,19 @@
-1class Solution {
-2public:
-3    string capitalizeTitle(string title) {
-4        int n = title.length();
-5        int i = 0;
-6        while (i < n) {
-7            int j = i; 
-8            while (j < n && title[j] != ' ') {
-9                title[j] = tolower(static_cast<unsigned char>(title[j]));
-10                j++;
-11            }            
-12            if (j - i > 2) {
-13                title[i] = toupper(static_cast<unsigned char>(title[i]));
-14            }
-15            i = j + 1;
-16        }
-17        return title;
-18    }
-19};
-20
+class Solution {
+public:
+    string capitalizeTitle(string title) {
+        int n = title.length();
+        int i = 0;
+        while (i < n) {
+            int j = i; 
+            while (j < n && title[j] != ' ') {
+                title[j] = tolower(static_cast<unsigned char>(title[j]));
+                j++;
+            }            
+            if (j - i > 2) {
+                title[i] = toupper(static_cast<unsigned char>(title[i]));
+            }
+            i = j + 1;
+        }
+        return title;
+    }
+};
