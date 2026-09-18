53726ce823e910648f509891b18816ff9eaea789
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-18 10:28:28 +0530

    Update 11. Container With Most Water.cpp

--- 01-Arrays-and-Hashing/11. Container With Most Water/11. Container With Most Water.cpp
+++ 01-Arrays-and-Hashing/11. Container With Most Water/11. Container With Most Water.cpp
@@ -1,21 +1,21 @@
-1class Solution {
-2public:
-3    int maxArea(vector<int>& height){
-4        int left=0;
-5        int right = height.size()-1;
-6        int max_area=0;
-7        while(left < right){
-8            int width = right - left;
-9            int current_height = min(height[left], height[right]);
-10            int  current_area = width * current_height;
-11            max_area = max(max_area, current_area);
-12            if (height[left] < height[right]){
-13                left++;
-14            } 
-15            else{
-16                right--;
-17            }
-18        }
-19        return max_area;
-20    }
-21};
+class Solution {
+public:
+    int maxArea(vector<int>& height){
+        int left=0;
+        int right = height.size()-1;
+        int max_area=0;
+        while(left < right){
+            int width = right - left;
+            int current_height = min(height[left], height[right]);
+            int  current_area = width * current_height;
+            max_area = max(max_area, current_area);
+            if (height[left] < height[right]){
+                left++;
+            } 
+            else{
+                right--;
+            }
+        }
+      return max_area;
+    }
+};
