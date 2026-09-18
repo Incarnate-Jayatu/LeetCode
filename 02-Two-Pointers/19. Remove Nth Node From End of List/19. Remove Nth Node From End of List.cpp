4bef1e25962c899f75db7e9f61a3fc609b2761ee
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-18 10:38:58 +0530

    Update 19. Remove Nth Node From End of List.cpp

--- 02-Two-Pointers/19. Remove Nth Node From End of List/19. Remove Nth Node From End of List.cpp
+++ 02-Two-Pointers/19. Remove Nth Node From End of List/19. Remove Nth Node From End of List.cpp
@@ -1,34 +1,33 @@
-1/**
-2 * Definition for singly-linked list.
-3 * struct ListNode {
-4 *     int val;
-5 *     ListNode *next;
-6 *     ListNode() : val(0), next(nullptr) {}
-7 *     ListNode(int x) : val(x), next(nullptr) {}
-8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-9 * };
-10 */
-11class Solution{
-12public:
-13    ListNode* removeNthFromEnd(ListNode* head, int n){
-14        ListNode* fast = head;
-15        ListNode* slow = head;
-16        for(int i = 0; i < n; ++i){
-17            fast = fast->next;
-18        }
-19        if(fast == nullptr){
-20            ListNode* newHead = head->next;
-21            delete head;
-22            return newHead;
-23        }
-24        while(fast->next != nullptr){
-25            fast = fast->next;
-26            slow = slow->next;
-27        }
-28        ListNode* nodeToDelete = slow->next;
-29        slow->next = slow->next->next;
-30        delete nodeToDelete;
-31        return head;
-32    }
-33};
-34
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution{
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n){
+        ListNode* fast = head;
+        ListNode* slow = head;
+        for(int i = 0; i < n; ++i){
+            fast = fast->next;
+        }
+        if(fast == nullptr){
+            ListNode* newHead = head->next;
+            delete head;
+            return newHead;
+        }
+        while(fast->next != nullptr){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        ListNode* nodeToDelete = slow->next;
+        slow->next = slow->next->next;
+        delete nodeToDelete;
+        return head;
+    }
+};
