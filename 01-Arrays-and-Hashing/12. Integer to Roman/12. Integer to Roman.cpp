3c09aabdcfb3500830df3fd5e775af88eba88a30
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-18 10:17:30 +0530

    Update 12. Integer to Roman.cpp

--- 01-Arrays-and-Hashing/12. Integer to Roman/12. Integer to Roman.cpp
+++ 01-Arrays-and-Hashing/12. Integer to Roman/12. Integer to Roman.cpp
@@ -1,14 +1,14 @@
-1class Solution {
-2public:
-3    string intToRoman(int num) {
-4        string Roman = ;
-5       vector<pair<int, string>> storeIntRoman = {{1000, M}, {900, CM}, {500, D}, {400, CD}, {100, C}, {90, XC}, {50, L}, {40, XL}, {10, X}, {9, IX}, {5, V}, {4, IV}, {1, I}};
-6        for (int i = 0; i < storeIntRoman.size(); i++) {
-7            while (num >= storeIntRoman[i].first) {
-8                Roman += storeIntRoman[i].second;
-9                num -= storeIntRoman[i].first;
-10            }
-11        }
-12        return Roman;
-13    }
-14};
+class Solution {
+public:
+    string intToRoman(int num) {
+        string Roman = ;
+       vector<pair<int, string>> storeIntRoman = {{1000, M}, {900, CM}, {500, D}, {400, CD}, {100, C}, {90, XC}, {50, L}, {40, XL}, {10, X}, {9, IX}, {5, V}, {4, IV}, {1, I}};
+        for (int i = 0; i < storeIntRoman.size(); i++) {
+            while (num >= storeIntRoman[i].first) {
+                Roman += storeIntRoman[i].second;
+                num -= storeIntRoman[i].first;
+            }
+        }
+        return Roman;
+    }
+};
