e593d03d865fa3da165a8558bad8b206d7d4ca76
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-18 11:09:53 +0530

    Update 121. Best Time to Buy and Sell Stock.cpp

--- 01-Arrays-and-Hashing/121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.cpp
+++ 01-Arrays-and-Hashing/121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.cpp
@@ -1,20 +1,18 @@
-1class Solution { 
-2public: 
-3    int maxProfit(vector<int>& prices) { 
-4        if (prices.empty()) return 0;
-5        
-6        int min = prices[0];       
-7        int max_profit = 0;         
-8        int n = prices.size() - 1;
-9        for(int i = 0; i <= n; i++){ 
-10            if(prices[i] < min){ 
-11                min = prices[i]; 
-12            } 
-13            else if(prices[i] - min > max_profit){ 
-14                max_profit = prices[i] - min; 
-15            } 
-16        } 
-17        return max_profit; 
-18    } 
-19};
-20
+class Solution { 
+public: 
+    int maxProfit(vector<int>& prices) { 
+        if (prices.empty()) return 0;    
+        int min = prices[0];       
+        int max_profit = 0;         
+        int n = prices.size() - 1;
+        for(int i = 0; i <= n; i++){ 
+            if(prices[i] < min){ 
+                min = prices[i]; 
+            } 
+            else if(prices[i] - min > max_profit){ 
+                max_profit = prices[i] - min; 
+            } 
+        } 
+        return max_profit; 
+    } 
+};
