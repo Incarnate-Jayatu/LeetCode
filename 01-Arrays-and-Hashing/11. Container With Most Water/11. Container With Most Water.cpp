8d5e755146dcc8f74f4a9d9a6cff849ec1b37bf1
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-18 10:24:54 +0530

    [11] [Time Beats: 38.36%] [Memory Beats: 97.88%] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/11. Container With Most Water/11. Container With Most Water.cpp
@@ -0,0 +1,20 @@
+1class Solution {
+2public:
+3    long long maxArea(vector<int>& height) {
+4        int left=0;
+5        int right = height.size()-1;
+6        int max_area=0;
+7        while (left < right) {
+8            int width = right - left;
+9            int current_height = min(height[left], height[right]);
+10            int  current_area = width * current_height;
+11            max_area = max(max_area, current_area);
+12            if (height[left] < height[right]) {
+13                left++;
+14            } else {
+15                right--;
+16            }
+17        }
+18        return max_area;
+19    }
+20};
