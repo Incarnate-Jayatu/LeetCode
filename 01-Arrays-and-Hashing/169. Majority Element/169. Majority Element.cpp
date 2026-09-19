91b8ac22998681a452661082165d1de92e300344
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-19 23:01:38 +0530

    Update 169. Majority Element.cpp

--- 01-Arrays-and-Hashing/169. Majority Element/169. Majority Element.cpp
+++ 01-Arrays-and-Hashing/169. Majority Element/169. Majority Element.cpp
@@ -1,20 +1,20 @@
-1class Solution{
-2public:
-3    int majorityElement(vector<int>& nums){
-4        int element = 0;
-5        int count = 0;
-6        for(int num : nums){
-7            if(count == 0){
-8                element = num;
-9            }
-10
-11            if(num == element){
-12                count++;
-13            }
-14            else{
-15                count--;
-16            }
-17        }
-18        return element;
-19    }
-20};
+class Solution{
+public:
+    int majorityElement(vector<int>& nums){
+        int element = 0;
+        int count = 0;
+        for(int num : nums){
+            if(count == 0){
+                element = num;
+            }
+
+            if(num == element){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return element;
+    }
+};
