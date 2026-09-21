9d88d622a48984d954a5b9cee2f3b10b20488f02
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-21 23:08:13 +0530

    Update 3959. Check Good Integer.java

--- 13-Math/3959. Check Good Integer/3959. Check Good Integer.java
+++ 13-Math/3959. Check Good Integer/3959. Check Good Integer.java
@@ -1,10 +1,10 @@
-1class Solution {
-2    public boolean checkGoodInteger(int n) {
-3        int s = 0;
-4        for (; n > 0; n /= 10) {
-5            int x = n % 10;
-6            s += x * (x - 1);
-7        }
-8        return s >= 50;
-9    }
-10}
+class Solution {
+    public boolean checkGoodInteger(int n) {
+        int s = 0;
+        for (; n > 0; n /= 10) {
+            int x = n % 10;
+            s += x * (x - 1);
+        }
+      return s >= 50;
+    }
+}
