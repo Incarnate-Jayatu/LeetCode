c910b6a860399095ef56e98fd3c76c7d656f5600
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-21 23:27:24 +0530

    Update 1822. Sign of the Product of an Array.java

--- 01-Arrays-and-Hashing/1822. Sign of the Product of an Array/1822. Sign of the Product of an Array.java
+++ 01-Arrays-and-Hashing/1822. Sign of the Product of an Array/1822. Sign of the Product of an Array.java
@@ -1,15 +1,14 @@
-1class Solution {
-2    public int arraySign(int[] nums){
-3        int sign = 1;
-4        for(int i = 0; i < nums.length; i++){
-5            if(nums[i] == 0){
-6                return 0;
-7            }
-8            if(nums[i] < 0){
-9                sign = -sign;
-10            }
-11        }
-12        return sign;
-13    }
-14}
-15
+class Solution {
+    public int arraySign(int[] nums){
+        int sign = 1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                return 0;
+            }
+            if(nums[i] < 0){
+                sign = -sign;
+            }
+        }
+        return sign;
+    }
+}
