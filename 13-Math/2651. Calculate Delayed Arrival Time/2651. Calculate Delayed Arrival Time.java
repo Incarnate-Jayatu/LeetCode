c92c43c152c4595bd37943f45c8ccea406278b0b
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-21 23:14:50 +0530

    [2651] [Time Beats: 100.00%] [Memory Beats: 78.42%] - Leet2Hub

--- 13-Math/2651. Calculate Delayed Arrival Time/2651. Calculate Delayed Arrival Time.java
+++ 13-Math/2651. Calculate Delayed Arrival Time/2651. Calculate Delayed Arrival Time.java
@@ -1,1 +1,8 @@
-class Solution {    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {        int delay = arrivalTime+delayedTime;        if(delay>=24)            delay = delay - 24;        return delay;    }}
+1class Solution {
+2    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+3        int delay = arrivalTime+delayedTime;
+4        if(delay>=24)
+5            delay = delay - 24;
+6        return delay;
+7    }
+8}
