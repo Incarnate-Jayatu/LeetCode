52fa56aa42e75b297ede4bc6497fe0e90c96b55b
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-23 11:52:24 +0530

    Update 1920. Build Array from Permutation.cpp

--- 01-Arrays-and-Hashing/1920. Build Array from Permutation/1920. Build Array from Permutation.cpp
+++ 01-Arrays-and-Hashing/1920. Build Array from Permutation/1920. Build Array from Permutation.cpp
@@ -1,11 +1,11 @@
-1class Solution {
-2public:
-3    vector<int> buildArray(vector<int>& nums) {
-4        int n = nums.size();          
-5        vector<int> ans(n);          
-6        for(int i = 0; i < n; i++){   
-7            ans[i] = nums[nums[i]];
-8        }
-9        return ans;
-10    }
-11};
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();          
+        vector<int> ans(n);          
+        for(int i = 0; i < n; i++){   
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+};
