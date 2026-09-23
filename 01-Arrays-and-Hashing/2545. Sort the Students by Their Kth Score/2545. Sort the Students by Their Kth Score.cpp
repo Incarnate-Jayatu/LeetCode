efe5cff94347b443c04bcc104adac4f26f56b299
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-23 12:27:53 +0530

    Update 2545. Sort the Students by Their Kth Score.cpp

--- 01-Arrays-and-Hashing/2545. Sort the Students by Their Kth Score/2545. Sort the Students by Their Kth Score.cpp
+++ 01-Arrays-and-Hashing/2545. Sort the Students by Their Kth Score/2545. Sort the Students by Their Kth Score.cpp
@@ -1,15 +1,14 @@
-1class Solution {
-2public:
-3    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
-4        int numberOfRows = score.size();
-5        for (int i = 0; i < numberOfRows; i++) {
-6            for (int j = i + 1; j < numberOfRows; j++) {
-7                if (score[j][k] > score[i][k]) {
-8                    swap(score[i], score[j]); 
-9                }
-10            }
-11        }
-12        return score;
-13    }
-14};
-15
+class Solution {
+public:
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        int numberOfRows = score.size();
+        for (int i = 0; i < numberOfRows; i++) {
+            for (int j = i + 1; j < numberOfRows; j++) {
+                if (score[j][k] > score[i][k]) {
+                    swap(score[i], score[j]); 
+                }
+            }
+        }
+        return score;
+    }
+};
