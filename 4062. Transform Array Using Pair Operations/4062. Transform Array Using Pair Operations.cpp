b45866796eb6a70639e4d0851b61c9239c7902ea
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-26 23:23:55 +0530

    Update 4062. Transform Array Using Pair Operations.cpp

--- 4062. Transform Array Using Pair Operations/4062. Transform Array Using Pair Operations.cpp
+++ 4062. Transform Array Using Pair Operations/4062. Transform Array Using Pair Operations.cpp
@@ -1,17 +1,17 @@
-1class Solution {
-2public:
-3    bool canTransform(vector<int>& source, vector<int>& target) {
-4        vector<int> dum = source;
-5        int n=dum.size();
-6        if(n == 1){
-7            return dum[0] == target[0];
-8        }
-9        long long sourceSum = 0;
-10        long long targetSum = 0;
-11        for(int i=0; i<n; i++){
-12            sourceSum += dum[i];
-13            targetSum += target[i];
-14        }
-15        return sourceSum == targetSum;
-16    }
-17};
+class Solution {
+public:
+    bool canTransform(vector<int>& source, vector<int>& target) {
+        vector<int> dum = source;
+        int n=dum.size();
+        if(n == 1){
+            return dum[0] == target[0];
+        }
+        long long sourceSum = 0;
+        long long targetSum = 0;
+        for(int i=0; i<n; i++){
+            sourceSum += dum[i];
+            targetSum += target[i];
+        }
+        return sourceSum == targetSum;
+    }
+};
