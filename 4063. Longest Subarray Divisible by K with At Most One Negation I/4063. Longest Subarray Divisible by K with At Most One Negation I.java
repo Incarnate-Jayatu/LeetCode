d9b9c85095f3a8a581c656e94c27899a7c8840bc
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-26 23:25:24 +0530

    [4063] [Time Beats: 32.21%] [Memory Beats: 16.35%] - Leet2Hub

--- 4063. Longest Subarray Divisible by K with At Most One Negation I/4063. Longest Subarray Divisible by K with At Most One Negation I.java
+++ 4063. Longest Subarray Divisible by K with At Most One Negation I/4063. Longest Subarray Divisible by K with At Most One Negation I.java
@@ -1,1 +1,21 @@
-class Solution {    public int longestSubarray(int[] nums, int k) {        int []minVal = nums;        int no = nums.length;        int maxLength = 0;        for(int i=0; i<no; i++){            long ongoingSum = 0;            Set<Integer> obsNegations = new HashSet<>();            for(int j=i; j<no; j++){                ongoingSum += nums[j];                int negRemainder = ((2*nums[j]%k+k)%k);                obsNegations.add(negRemainder);                int ongoingRemainder = (int)((ongoingSum%k+k)%k);                if(ongoingRemainder == 0 || obsNegations.contains(ongoingRemainder)){                    maxLength = Math.max(maxLength, j-i+1);                }            }        }
+1class Solution {
+2    public int longestSubarray(int[] nums, int k) {
+3        int []minVal = nums;
+4        int no = nums.length;
+5        int maxLength = 0;
+6        for(int i=0; i<no; i++){
+7            long ongoingSum = 0;
+8            Set<Integer> obsNegations = new HashSet<>();
+9            for(int j=i; j<no; j++){
+10                ongoingSum += nums[j];
+11                int negRemainder = ((2*nums[j]%k+k)%k);
+12                obsNegations.add(negRemainder);
+13                int ongoingRemainder = (int)((ongoingSum%k+k)%k);
+14                if(ongoingRemainder == 0 || obsNegations.contains(ongoingRemainder)){
+15                    maxLength = Math.max(maxLength, j-i+1);
+16                }
+17            }
+18        }
+19        return maxLength;
+20    }
+21}
