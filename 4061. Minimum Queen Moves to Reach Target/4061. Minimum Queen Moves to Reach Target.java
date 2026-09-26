fd003c117f487b8c06f3acbdefc4eac296934ad2
Jayatu Avinash Bhattad <jayatu29@gmail.com>
2026-09-26 23:21:54 +0530

    Update 4061. Minimum Queen Moves to Reach Target.java

--- 4061. Minimum Queen Moves to Reach Target/4061. Minimum Queen Moves to Reach Target.java
+++ 4061. Minimum Queen Moves to Reach Target/4061. Minimum Queen Moves to Reach Target.java
@@ -1,14 +1,14 @@
-1class Solution {
-2    public int minQueenMoves(int[] source, int[] target) {
-3        int rowOne = source[0], colOne = source[1];
-4        int rowTwo = target[0], colTwo = target[1];
-5        if(rowOne == rowTwo && colOne == colTwo)
-6        {
-7             return 0;   
-8        }
-9        if(rowOne == rowTwo || colOne == colTwo || Math.abs(rowOne-rowTwo) == Math.abs(colOne-colTwo)){
-10             return 1;   
-11        }
-12        return 2;
-13    }
-14}
+class Solution {
+    public int minQueenMoves(int[] source, int[] target) {
+        int rowOne = source[0], colOne = source[1];
+        int rowTwo = target[0], colTwo = target[1];
+        if(rowOne == rowTwo && colOne == colTwo)
+        {
+             return 0;   
+        }
+        if(rowOne == rowTwo || colOne == colTwo || Math.abs(rowOne-rowTwo) == Math.abs(colOne-colTwo)){
+             return 1;   
+        }
+        return 2;
+    }
+}
